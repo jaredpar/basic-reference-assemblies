d2ac71f03f878d26cf8d9a6b76830e4ce06aa7d2
Jared Parsons <jared@popcornbear.org>
2023-08-22 13:23:28 -0700

    More

--- Scripts/Generate.ps1
+++ Scripts/Generate.ps1
@@ -66,7 +66,7 @@ namespace Basic.Reference.Assemblies
     $dllPath = $targetsPrefix + $dllPath
 
     $targetsContent += @"
-        <EmbeddedResource Include="$dllPath">
+        <EmbeddedResource Include="$dllPath" WithCulture="false">
           <LogicalName>$logicalName</LogicalName>
           <Link>Resources\$lowerName\$dllName</Link>
         </EmbeddedResource>
